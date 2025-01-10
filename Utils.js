fa787e508740c895d3ca3b74505c29af86c9a0b0
nachoflorida <90484091+nachoflorida@users.noreply.github.com>
2025-01-10 11:19:20 +0100

    Delete Utils.js

--- Utils.js
+++
@@ -1,5 +0,0 @@
-export default function calculaLetraNif(unDni) {
-    let arrayDeLetras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'];
-    let pos = unDni % 23;
-    return arrayDeLetras[pos];
-}
