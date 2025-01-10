7594c9671da7abf0d0ff9bdd5ace439e9d74bb7e
nachoflorida <90484091+nachoflorida@users.noreply.github.com>
2025-01-10 11:20:03 +0100

    Updated styles to show them properly on Expo Snack

--- components/Nif.js
+++ components/Nif.js
@@ -1,55 +1,83 @@
+// Nif.js
 import React, { Component } from 'react';
-import { View, Text, TextInput, Keyboard } from 'react-native';
+import { View, Text, TextInput, Keyboard, StyleSheet } from 'react-native';
 
-import calculaLetraNif from '../Utils';
+const calculaLetraNif = (unDni) => {
+  const arrayDeLetras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'];
+  return arrayDeLetras[unDni % 23];
+};
 
 export default class Nif extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            elDNIintroducido: undefined,
-            letraDNI: undefined,
-        };
-    }
-    guardaDNI = valor => {
-        if (valor.length === 8) {
-            this.setState({ elDNIintroducido: valor });
-            let laLetra = calculaLetraNif(valor);
-            this.setState({ letraDNI: laLetra });
-            Keyboard.dismiss();
-            console.log("En el componente hijo, el dni es " + valor + " i la letra es " + laLetra);
-            this.props.devuelveNif({ dni: valor, letra: laLetra }); // Nos comunicamos con el componente Padre
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      elDNIintroducido: '',
+      letraDNI: '',
     };
+  }
 
-    render() {
-        return (
-            <View
-                style={{
-                    flexDirection: 'row', justifyContent: 'center', alignItems: 'center',
-                    padding: 10, backgroundColor: this.props.color, borderColor: 'blue', borderWidth: 2,
-                }}>
-                <Text>DNI:</Text>
-                <TextInput
-                    style={{ fontSize: 15, fontWeight: "normal", width: 200 }}
-                    placeholder="Escribe el dni"
-                    placeholderTextColor="#FF0000"
-                    onChangeText={this.guardaDNI}
-                    maxLength={8}
-                    keyboardType="number-pad"
-                    underlineColorAndroid="blue"
-                />
-
-                <Text>Letra:</Text>
-                <TextInput
-                    style={{ fontWeight: 'bold', fontSize: 15 }}
-                    placeholder=""
-                    placeholderTextColor="#FF0000"
-                    value={this.state.letraDNI}
-                    editable={false}
-                    underlineColorAndroid="blue"
-                />
-            </View>
-        );
+  guardaDNI = (valor) => {
+    if (valor.length === 8) {
+      const letra = calculaLetraNif(valor);
+      this.setState({ elDNIintroducido: valor, letraDNI: letra });
+      Keyboard.dismiss();
+      this.props.devuelveNif({ dni: valor, letra });
+    } else {
+      this.setState({ elDNIintroducido: '', letraDNI: '' });
     }
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.label}>DNI:</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Escribe el DNI"
+          placeholderTextColor="#ff0000"
+          onChangeText={this.guardaDNI}
+          maxLength={8}
+          keyboardType="number-pad"
+        />
+        <Text style={styles.label}>Letra:</Text>
+        <TextInput
+          style={styles.letraInput}
+          value={this.state.letraDNI}
+          editable={false}
+        />
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    padding: 10,
+    borderColor: 'blue',
+    borderWidth: 2,
+  },
+  label: {
+    fontWeight: '600',
+    marginHorizontal: 5,
+    color: '#333',
+  },
+  input: {
+    width: 120,
+    padding: 8,
+    borderBottomWidth: 1,
+    borderBottomColor: '#999',
+    marginHorizontal: 5,
+  },
+  letraInput: {
+    width: 40,
+    padding: 8,
+    borderBottomWidth: 1,
+    borderBottomColor: '#999',
+    marginHorizontal: 5,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+});
